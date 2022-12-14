8ea9e48d61a4004e07e6409a337afe2f7ab154ba
C Martin <cmart131@yahoo.com>
2022-12-14 12:55:43 -0800

    going through API blog post to figure out how to set up the weather API into JS

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 var APIKey = "db7246c26dae7066dbd3873f47ec1b21";
-
-
+//API call for key
 api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+ 
+//API call for city/Country
+https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid={API key}
+ var city;
+
+ var queryURL="http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+ 
+ fetch(queryURL)
