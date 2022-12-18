dad94614b5ebe84e78a059f6fed676fc840839d8
C Martin <cmart131@yahoo.com>
2022-12-17 19:30:12 -0800

    FINALLY got the current weather conditions to show up upon search. next is local storage

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 var APIKey = "db7246c26dae7066dbd3873f47ec1b21";
-var city=" ";
+var currentCity=" ";
+var lastCity = " ";
 var searchButton = document.getElementById("search");
-var searchHistory = document.getElementById("datalistOptions")
+var searchHistory = document.getElementById("search-history");
+var citySearch = document.getElementById("city-search");
+var fiveDay = document.getElementById("five-day");
 
  
-function getApi() {
- 
+function getCurrentWeather(city) {
+    var city = $('#city-search').val().trim();
+    currentCity= $('#city-search').val();
     var queryURL="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
   
     fetch(queryURL)
@@ -14,9 +18,25 @@ function getApi() {
       })
       .then(function (data) {
         console.log(data);
+
+        var weatherIcon = data.weather[0].icon;
+        var iconURL = `https://openweathermap.org/img/w/${weatherIcon}.png`;
+
+        var currentCity = $(`
+            <h2 id="currentCity">
+                ${data.name} <img src="${iconURL}" alt="${data.weather[0].description}" />
+            </h2>
+            <p>Temperature: ${data.main.temp} °F</p>
+            <p>Humidity: ${data.main.humidity}\%</p>
+            <p>Wind Speed: ${data.wind.speed} MPH</p>
+        `);
+
+        $("#current-city").append(currentCity);
       });
+        
   }
-  searchButton.addEventListener('click', getApi);
+  searchButton.addEventListener('click', getCurrentWeather);
 
 
+ 
  
