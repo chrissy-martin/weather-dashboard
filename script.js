a63dece5d2bffa3fa4fc3707a7c0db74958cd0a3
C Martin <cmart131@yahoo.com>
2022-12-17 18:12:45 -0800

    working on js file. struggling. can't figure out how to get the api to work properly. will be working on local storage next

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 var APIKey = "db7246c26dae7066dbd3873f47ec1b21";
- var city;
+var city=" ";
+var searchButton = document.getElementById("search");
+var searchHistory = document.getElementById("datalistOptions")
 
- var queryURL="http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
  
- fetch(queryURL)
+function getApi() {
+ 
+    var queryURL="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+  
+    fetch(queryURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+      });
+  }
+  searchButton.addEventListener('click', getApi);
+
+
+ 
